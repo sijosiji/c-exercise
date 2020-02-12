87a7bcb6be968ecfb29c5d6c2076a5b661aeabc5
sijo Jacob <sijosoljacob>
2020-02-12 14:33:49 -0700

    excerise c sharp

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c__excersise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sijo jacob!");
+        }
+    }
+}
